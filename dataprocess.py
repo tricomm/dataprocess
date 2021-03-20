ad745b1e64c3ed6831164505d72daa23f669f323
tricomm <1010891455@qq.com>
2021-03-20 13:09:43 +0800

    开始写插值

--- dataprocess.py
+++ dataprocess.py
@@ -1,7 +1,12 @@
 import re
 class MPdataprocess:
     def __init__(self):
+        self.data=[[]for i in range(7)]
         pass
+
+    def liner_intert(self,t,t1,x1,t2,x2):
+        return x1 + ((t-t1)/(t2-t1))*(x2-x1)
+
 
     def perprocess(self,filedir):
         alread_feature=0
@@ -23,16 +28,57 @@ class MPdataprocess:
                 elif l_list[0]=='GYRO':
                     if gyro == None:
                         alread_feature+=1
-                    acce = l_list
+                    gyro = l_list
                 elif l_list[0] == 'MAGN':
-                    if acce == None:
+                    if magn == None:
                         alread_feature+=1
-                    acce = l_list
+                    magn = l_list
                 elif l_list[0] == 'PRES':
-                    if acce == None:
+                    if pres == None:
                         alread_feature+=1
-                    acce = l_list
+                    pres = l_list
                 elif l_list[0] == 'AHRS':
-                    if acce == None:
+                    if ahrs == None:
                         alread_feature+=1
-                    acce = l_list
+                    ahrs = l_list
+                if alread_feature == 5:
+                    self.begin_time = l_list[2]
+
+    def load(self,filedir):
+        # acce gyro magn pres ahrs wifi gnss
+    #data   0    1    2    3    4   5     6
+    #以上为数据对应关系data 为7*n的矩阵
+        with open(filedir) as file_object:
+            for line in file_object:
+                l_list = re.split(r'[;\s]', line.strip())
+                #需要读入buffer的数据
+                if l_list[0]!='%' and l_list[2]>=self.begin_time:
+                    #不同类型传感器存放位置不同 对齐不同
+                    if l_list[0] == 'ACCE':
+                        self.data[0].append(l_list)
+                    elif l_list[0] == 'GYRO':
+                        self.data[1].append(l_list)
+                    elif l_list[0] == 'MAGN':
+                        self.data[2].append(l_list)
+                    elif l_list[0] == 'PRES':
+                        self.data[3].append(l_list)
+                    elif l_list[0] == 'AHRS':
+                        self.data[4].append(l_list)
+                    elif l_list[0] == 'WIFI':
+                        l = len(self.data[5])
+                        ##去重
+                        if (l>0 and self.data[5][l-1] != l_list) or l==0:
+                                self.data[5].append(l_list)
+                    elif l_list[0] == 'GNSS':
+                        l = len(self.data[6])
+                        if (l > 0 and self.data[6][l - 1] != l_list) or l == 0:
+                            self.data[6].append(l_list)
+        #对齐时间
+        for i in range(len(self.data)):
+            if i<5:
+                d = self.data[i][0][2]-self.begin_time
+                for i in self.data[i]:
+                    i[2]=i[2]-d;
+        self.end_time =
+        #插值
+        #需要的数据
